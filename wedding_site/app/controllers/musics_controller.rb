271c0af65e3e65bdd3fda5c2ed36879a70dec1e7
bob <bobreilly3@gmail.com>
2016-07-26 13:28:15 -0400

    test

--- wedding_site/app/controllers/musics_controller.rb
+++ wedding_site/app/controllers/musics_controller.rb
@@ -1,33 +1,45 @@
 class MusicsController < ApplicationController
  before_action :define_object, only: [:show, :edit, :update, :destroy]
-def index
-	@music = Music.all
-end
-
-def show
-
-end
-
-def create
-	@music = Music.new(music_params)
-	respond_to do |format|
-	   if @music.save
-		format.html{redirect_to @music, notice: "Entry successfully added to playlist"}
-		format.json { render :show, status: :created, location: @music}
-	   end
+ before_action :require_login
+	def index
+		@music = Music.all
 	end
-end
 
-def music_params
-	params.require(:music).permit(:band, :track)
-end
+	def show
+	end
 
-def destroy
-	Music.find(params[:id]).destroy
-end
+	def create
+		@music = Music.new(music_params)
+		respond_to do |format|
+	   		if @music.save
+				format.html{redirect_to @music, notice: "Entry successfully added to playlist"}
+				format.json { render :index, status: :created, location: @music}
+	   		end
+		end
+	end
 
-def define_object
-	@music = Music.find(params[:id])
-end
+	def music_params
+		params.require(:music).permit(:band, :track)
+	end
+
+	def destroy
+		@music.destroy
+		respond_to do |format|
+			
+				format.html{redirect_to @music, notice: "Entry successfully removed from playlist"}
+				format.json {render :index, status: created, location: @music}
+		end
+	end
+
+	def define_object
+		@music = Music.find(params[:id])
+	end
+	
+	def require_login
+		if current_user == nil
+			redirect_to :controller => 'landing' 
+		end
+	end
+
 
 end
