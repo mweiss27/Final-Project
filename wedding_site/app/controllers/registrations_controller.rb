ee1a474b1df3a6d0645dc3fbde060610b3e8da32
Matt <m.weiss27@yahoo.com>
2016-07-26 14:12:46 -0400

    abc

--- wedding_site/app/controllers/registrations_controller.rb
+++ wedding_site/app/controllers/registrations_controller.rb
@@ -2,15 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     super
-    puts "current_user: #{current_user}"
     @person = Person.new(:first_name => params["first_name"], :last_name => params["last_name"])
     @person.save
-    puts "current_user: #{current_user}"
     current_user.person_id = @person.id
     current_user.save
   end
 
-  def after_create
-    puts "AFTER CREATE"
-  end
 end 
